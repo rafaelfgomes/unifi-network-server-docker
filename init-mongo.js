6ad1777d0a8142aa777f34880377e11a0584684f
Rafael Ferreira Gomes <faelfg@gmail.com>
2025-04-22 14:37:25 -0300

    add init mongo file

---
+++ init-mongo.js
@@ -0,0 +1,2 @@
+db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "Wc66c4PSkjtMdXacUzVjM79Py", roles: [{role: "dbOwner", db: "unifi"}]});
+db.getSiblingDB("unifi_stat").createUser({user: "unifi", pwd: "Wc66c4PSkjtMdXacUzVjM79Py", roles: [{role: "dbOwner", db: "unifi_stat"}]});
