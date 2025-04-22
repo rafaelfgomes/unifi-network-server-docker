21079bc8171d43a8b3991c87856efaea9e0e9cb1
Rafael Ferreira Gomes <faelfg@gmail.com>
2025-04-22 20:43:24 -0300

    rollback mongo changes

--- init-mongo.js
+++
@@ -1,2 +0,0 @@
-db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "Wc66c4PSkjtMdXacUzVjM79Py", roles: [{role: "dbOwner", db: "unifi"}]});
-db.getSiblingDB("unifi_stat").createUser({user: "unifi", pwd: "Wc66c4PSkjtMdXacUzVjM79Py", roles: [{role: "dbOwner", db: "unifi_stat"}]});
